6adc14fc3d06dc83f3c58beeccccec666ba00e10
Gulilil <juan.csantoso@gmail.com>
2023-02-09 19:49:10 +0700

    explore?

--- src/main/java/Agents/Ling/Strategy/Explorer.java
+++ src/main/java/Agents/Ling/Strategy/Explorer.java
@@ -1,11 +1,22 @@
 package Agents.Ling.Strategy;
 
+import Actions.SearchFood;
 import Agents.Ling.StrategyInterface;
 import Models.PlayerAction;
+import Services.GameWatcherManager;
 
 public class Explorer implements StrategyInterface {
     @Override
     public PlayerAction computeNextAction() {
+        var watcher = GameWatcherManager.getWatcher();
+        PlayerAction act = new PlayerAction();
+        act.setPlayerId(watcher.player.getId());
+
+        var food = SearchFood.closestSafestFood();
+        if (food == null){
+
+        }
+
         return null;
     }
 
@@ -13,4 +24,5 @@ public class Explorer implements StrategyInterface {
     public int getPriorityLevel() {
         return 0;
     }
+    
 }
