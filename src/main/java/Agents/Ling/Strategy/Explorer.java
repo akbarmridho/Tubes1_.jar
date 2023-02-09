f71ed0750d8a78c38c1ca47ae8c8aa30c056236e
Gulilil <juan.csantoso@gmail.com>
2023-02-09 20:22:37 +0700

    explorer yang udah jadi ama Akbar

--- src/main/java/Agents/Ling/Strategy/Explorer.java
+++ src/main/java/Agents/Ling/Strategy/Explorer.java
@@ -1,28 +1,46 @@
 package Agents.Ling.Strategy;
 
 import Actions.SearchFood;
+import Agents.Ling.Priority;
 import Agents.Ling.StrategyInterface;
+import Enums.PlayerActions;
 import Models.PlayerAction;
+import Services.GameWatcher;
 import Services.GameWatcherManager;
+import Utils.Math;
 
 public class Explorer implements StrategyInterface {
+    public static final int SHIP_SIZE_CRITICAL = 20;
+    public static final int SHIP_SIZE_IDEAL = 50;
+    private GameWatcher watcher= GameWatcherManager.getWatcher();
+
     @Override
     public PlayerAction computeNextAction() {
-        var watcher = GameWatcherManager.getWatcher();
         PlayerAction act = new PlayerAction();
-        act.setPlayerId(watcher.player.getId());
 
-        var food = SearchFood.closestSafestFood();
-        if (food == null){
-
+        var Food = SearchFood.closestSafestFood();
+        if (Food == null){
+            Food = SearchFood.closestFood();
+            if (Food == null){
+                // act.setAction(PlayerActions.STOP);
+                return null;
+            }
         }
+        act.setHeading(Math.getHeadingBetween(watcher.player, Food));
+        act.setAction(PlayerActions.FORWARD);
 
-        return null;
+        return act;
     }
 
     @Override
     public int getPriorityLevel() {
-        return 0;
+        if (watcher.player.size <= SHIP_SIZE_CRITICAL){
+            return Priority.HIGH;
+        } else if (watcher.player.size <= SHIP_SIZE_IDEAL){
+            return Priority.NORMAL;
+        } else {
+            return Priority.LOW;
+        }
     }
     
 }
--- src/main/java/Agents/Ling/StrategyInterface.java
+++ src/main/java/Agents/Ling/StrategyInterface.java
@@ -5,7 +5,6 @@ import Services.GameWatcher;
 import Services.GameWatcherManager;
 
 public interface StrategyInterface extends Agent {
-    GameWatcher watcher = GameWatcherManager.getWatcher();
 
     public int getPriorityLevel();
 }
